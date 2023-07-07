159621a07fa72a9998be027399392de32b2f8aed
George Tegos <george.tegos.22@gmail.com>
2023-07-07 11:49:17 +0100

    answer 7th question

--- week_01_homework/users_exercise.py
+++ week_01_homework/users_exercise.py
@@ -82,7 +82,11 @@ for number in avril_list:
         new_list.append(number)
 print(new_list)
 
-# 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+# 7. Erik is one lottery number short! Add the number `7` 
+# to be included in his lottery numbers
+users["Erik"]["lottery_numbers"].append(7)
+print(users["Erik"]["lottery_numbers"])
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
