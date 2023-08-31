73a120613cfb8428da0dd82cfd4b0da3265c7a05
George Tegos <george.tegos.22@gmail.com>
2023-08-31 22:28:14 +0100

    frontend setup init d1

---
+++ hotel_app/client/src/containers/BookingsContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function BookingsContainer() {
+  return (
+    <div className='bookings-container'>
+        <div>Header</div>
+        <div>Hero</div>
+
+
+
+
+    </div>
+  )
+}
+
+export default BookingsContainer
