0b5b4638535cf2c7cb8f0bcfc749734a12b06e4b
George Tegos <george.tegos.22@gmail.com>
2023-09-24 12:22:26 +0100

    Plane class and PlaneTypes

--- Airlines_homework/src/main/java/org/example/Plane.java
+++ Airlines_homework/src/main/java/org/example/Plane.java
@@ -1,5 +1,21 @@
 package org.example;
 
 public class Plane {
+    private final PlaneType type;
 
+    public Plane(PlaneType type){
+        this.type = type;
+    }
+
+    public PlaneType getType() {
+        return type;
+    }
+
+    public double getPlaneCapacity(){
+        return type.getCapacity();
+    }
+
+    public double getPlateTotalWeight(){
+        return this.type.getTotalWeight();
+    }
 }
---
+++ Airlines_homework/src/test/java/PlaneTest.java
@@ -0,0 +1,26 @@
+import org.example.Plane;
+import org.example.PlaneType;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class PlaneTest {
+
+    private Plane plane;
+
+    @Before
+    public void before(){
+        plane = new Plane(PlaneType.Boeing747);
+    }
+
+    @Test
+    public void hasCapacity(){
+        double expect = 250;
+        assertEquals(expect,plane.getPlaneCapacity(), 0.0);
+    }
+
+    @Test
+    public void hasCorrectTotalWeight(){
+        double expect = plane.getPlateTotalWeight();
+        assertEquals(expect,PlaneType.Boeing747.getTotalWeight(), 0.0);
+    }
+}
