943c1e83f2d79da7f7ccf724f0ef417b84dc50da
George Tegos <george.tegos.22@gmail.com>
2023-09-29 20:46:06 +0100

    end of WK homework

--- TeaBiscuitsSpring/tea_and_biscuits_vite/src/components/BiscuitList.jsx
+++ TeaBiscuitsSpring/tea_and_biscuits_vite/src/components/BiscuitList.jsx
@@ -1,4 +1,9 @@
-const BiscuitList = ({ biscuits, handleBiscuitDelete }) => {
+import { useState } from "react";
+import EditBiscuitForm from "./EditBiscuitForm";
+
+const BiscuitList = ({ biscuits, handleBiscuitDelete, fetchBiscuits }) => {
+  const [edit, setEdit] = useState(false);
+
   return (
     <div id="biscuit-list">
       <h2>All the biccys!</h2>
@@ -10,6 +15,13 @@ const BiscuitList = ({ biscuits, handleBiscuitDelete }) => {
               <button onClick={() => handleBiscuitDelete(biscuit)}>
                 Delete
               </button>
+              <button onClick={() => setEdit(!edit)}>Edit</button>
+              {edit == true && (
+                <EditBiscuitForm
+                  fetchBiscuits={fetchBiscuits}
+                  biscuit={biscuit}
+                />
+              )}
             </li>
           );
         })}
---
+++ TeaBiscuitsSpring/tea_and_biscuits_vite/src/components/EditBiscuitForm.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { useState } from "react";
+
+function EditBiscuitForm({ biscuit, fetchBiscuits }) {
+  const [name, setName] = useState(biscuit.name);
+  const [brand, setBrand] = useState(biscuit.brand);
+  const BASE_URL = "http://localhost:8080";
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleBrandChange = (event) => {
+    setBrand(event.target.value);
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.value);
+    const config = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        brand,
+      }),
+    };
+
+    setBrand("");
+    setName("");
+
+    fetch(`${BASE_URL}/api/biscuits/${biscuit.id}`, config)
+      .then((res) => res.json())
+      .then((data) => fetchBiscuits());
+  };
+  return (
+    <div>
+      <form onSubmit={handleEdit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+        />
+
+        <label htmlFor="brand">Brand:</label>
+        <input
+          type="text"
+          name="brand"
+          value={brand}
+          onChange={handleBrandChange}
+        />
+        <input
+          type="submit"
+          value="Save"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default EditBiscuitForm;
